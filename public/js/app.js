962bea3ca2453bc72ff698ea9e1f9140866b8fcc
Yasin Atılkan <yasinatilgan60@gmail.com>
2021-03-22 21:08:57 +0300

    Set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Lütfen bekleyin...'
   messageTwo.textContent = ''
 
-  fetch('http://localhost:3000/weather?address='+location)
+  fetch('/weather?address='+location)
   .then((response) => {
     response.json().then((data) => {
       if(data.error){
--- src/app.js
+++ src/app.js
@@ -1,55 +1,56 @@
-const path = require("path"); // node core module
-const express = require("express"); // npm module
-const hbs = require("hbs"); // npm module
-const geocode = require("./utils/geocode");
-const forecast = require("./utils/forecast");
+const path = require("path") // node core module
+const express = require("express") // npm module
+const hbs = require("hbs") // npm module
+const geocode = require("./utils/geocode")
+const forecast = require("./utils/forecast")
+const port = process.env.PORT || 3000
 
-const app = express();
+const app = express()
 
 // Define paths for Express Config
-const publicDirectoryPath = path.join(__dirname, "../public"); // path manipulation
-const viewPath = path.join(__dirname, "../templates/views");
-const partialPath = path.join(__dirname, "../templates/partials");
+const publicDirectoryPath = path.join(__dirname, "../public") // path manipulation
+const viewPath = path.join(__dirname, "../templates/views")
+const partialPath = path.join(__dirname, "../templates/partials")
 
 // Setup handlebars engine and views location
-app.set("view engine", "hbs");
-app.set("views", viewPath);
-hbs.registerPartials(partialPath);
+app.set("view engine", "hbs")
+app.set("views", viewPath)
+hbs.registerPartials(partialPath)
 
-app.use(express.static(publicDirectoryPath));
+app.use(express.static(publicDirectoryPath))
 
 app.get("", (req, res) => {
   res.render("index", {
     title: "Hava Durumu",
     name: "Yasin Atılkan",
-  }); // views/index.hbs
-});
+  }) // views/index.hbs
+})
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "Uygulama Hakkında",
     name: "Yasin Atılkan",
-  });
-});
+  })
+})
 
 
 app.get("/products", (req, res) => {
   if (!req.query.search) {
     return res.send({
       error: "Bir arama ifadesi girmelisiniz.",
-    });
+    })
   }
   res.send({
     products: [],
-  });
-});
+  })
+})
 
 // endpoint
 app.get("/weather", (req, res) => {
   if (!req.query.address) {
     return res.send({
       error: "Bir adres ifadesi giriniz.",
-    });
+    })
   }
   geocode(
     req.query.address,
@@ -57,7 +58,7 @@ app.get("/weather", (req, res) => {
       if (error) {
         return res.send({
           error,
-        });
+        })
       }
       forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
@@ -67,13 +68,13 @@ app.get("/weather", (req, res) => {
           forecast: forecastData,
           location,
           address: req.query.address,
-        });
-        console.log(location);
-        console.log(forecastData);
-      });
+        })
+        console.log(location)
+        console.log(forecastData)
+      })
     }
-  );
-});
+  )
+})
 
 app.get("/about/*", (req, res) => {
   res.render("404", {
@@ -88,9 +89,9 @@ app.get("*", (req, res) => {
     title: "404",
     errorMessage: "Sayfa bulunamadı.",
     name: "Yasin Atılkan",
-  });
-});
+  })
+})
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
-});
+app.listen(port, () => {
+  console.log("Server is up on port "+port)
+})
